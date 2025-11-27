b781424fa13e69078b0fa51b3ca3bd72481ebe36
lorenzo-mazza <Lorenzo.mazza@ukdd.de>
2025-11-27 18:13:21 +0100

    Debugging missing expert usage picture

--- src/refactoring/training/callbacks.py
+++ src/refactoring/training/callbacks.py
@@ -196,6 +196,8 @@ class ConfusionMatrixCallback(Callback):
                 )
             plt.close(fig)
         expert_usages = pl_module.train_metrics.compute_expert_usage()
+        print(f"Expert usages at epoch end: {expert_usages}")
+        print(f"Metadata : {pl_module.train_metrics.metadata}")
         if expert_usages is not None:
             for key, expert_usage in expert_usages.items():
                 fig = self._create_expert_usage_figure(expert_usage, f"Train {key}")
