5dfb3aba2794595291ed19a12871368124a1197c
Rodríguez Jiménez <rodriguar@g27vmsteffi.med.tu-dresden.de>
2026-01-26 18:03:56 +0100

    adding developer

--- src/versatil/__init__.py
+++ src/versatil/__init__.py
@@ -1,8 +1,15 @@
 """Surg-IL library."""
 import warnings
+import debugpy
 
 warnings.filterwarnings(
     "ignore",
     category=UserWarning,
     module="pydantic._internal._generate_schema",
 )
+   
+# Listen on all interfaces
+#debugpy.listen(("0.0.0.0", 5678))
+print("Waiting for debugger to attach...")
+#debugpy.wait_for_client()
+print("Debugger attached!")
