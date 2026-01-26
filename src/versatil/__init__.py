32abea68e514d37b4efbc82a4a28f23336262d4d
Rodríguez Jiménez <rodriguar@g27vmsteffi.med.tu-dresden.de>
2026-01-26 18:05:35 +0100

    removing unused debugger

--- src/versatil/__init__.py
+++ src/versatil/__init__.py
@@ -1,15 +1,8 @@
 """Surg-IL library."""
 import warnings
-import debugpy
 
 warnings.filterwarnings(
     "ignore",
     category=UserWarning,
     module="pydantic._internal._generate_schema",
 )
-   
-# Listen on all interfaces
-#debugpy.listen(("0.0.0.0", 5678))
-print("Waiting for debugger to attach...")
-#debugpy.wait_for_client()
-print("Debugger attached!")
